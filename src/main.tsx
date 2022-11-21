00f825aa40ed35bf476f6156d6dcb4f9c7c6649d
Lucas <ilucas_@live.com>
2022-11-21 14:53:37 +0000

    remove css path

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
-import './styles/tailwind.css'
 import './i18n/index';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(

30cda2cf9e013f689feb3de3ea80d2908cee8acf
Lucas <ilucas_@live.com>
2022-11-21 14:50:51 +0000

    Install i18n and i18n-react

---
+++ src/i18n/index.ts
@@ -0,0 +1,17 @@
+import i18n from 'i18next';
+import { initReactI18next } from 'react-i18next'
+import ptJson from './translations/pt/pt.json'
+import enJson from './translations/en/en.json'
+
+i18n.use(initReactI18next).init({
+    fallbackLng: "pt",
+    interpolation: {
+        escapeValue: false,
+    },
+    resources: {
+        pt: ptJson,
+        en: enJson
+    }
+})
+
+export default i18n;
